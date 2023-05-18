d963f29389d7de042765116480e8943bfb8f5513
Victor Mont'Mor <victorperial@hotmail.com>
2023-05-18 16:49:48 -0300

    pipe

--- FlappyBird.py
+++ FlappyBird.py
@@ -63,7 +63,7 @@ Class Bird:
             if self.angle > -90:    
                 self.angle -= self.ROTATE_SPEED 
     
-    def draw(self):
+    def draw(self, screen):
         #definir qual imagem do passaro usar
         self.num_img += 1
         
@@ -86,13 +86,59 @@ Class Bird:
             self.num_img = self.ANIMATION_TIME*2 
         
         #desenhar imagem
+        rotate_img = pygame.transform.rotate(self.img, self.angle) #rotaciona a imagem
+        center_img = self.img.get_rect(topleft=(self.x, self.y).center)
+        rectangle = rotate_img.get_rect(center = center_img)
+        screen.blit(rotate_img, rectangle.topleft)
         
-        
-        
+    #método para criar uma mascara no pássaro e evitar colisões em que nao houve contato de fato
+    def get_mask(self):
+        pygame.mask.frok_surface(self.img)
         
         
 Class Pipe:
-    pass
-
+    DISTANCE = 200 # distancia entre o cano de cima e o de baixo
+    SPEED = 5 # nesse código, o cano que se movimenta para trás, e o pássaro fica parado no eixo y
+    
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top_position = 0
+        self.bottom_position = 0
+        self.PIPE_TOP = pygame.transform.flip(IMG_PIPE, False, True)
+        self.PIPE_BOTTOM = IMG_PIPE
+        self.overtook = False
+        self.define_height()
+        
+    def define_height(self):
+        self.height = random.randrange(50, 450)
+        self.top_position = self.height - self.PIPE_TOP.get_height()
+        self.bottom_position = self.height + self.DISTANCE
+        
+    def move(self):
+        self.x -= self.VELOCIDADE
+    
+    def draw(self, screen):
+        screen.blit(self.PIPE_TOP, (self.x, self.top_position))
+        screen.blit(self.PIPE_BOTTOM, (self.x, self.bottom_position))
+    
+    def colision(self, bird):
+        
+        # pega a máscara do pássado, cano de cima e cano de baixo, verifica se há uma sobreposição entre elas, e retorna o resultado
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        
+        top_distance = (self.x - bird.x, self.top_position - round(bird.y))
+        bottom_distance = (self.x - bird.x, self.bottom_position - round(bird.y))
+        
+        top_colision = bird_mask.overlap(top_mask, top_distance)
+        bottom_colision = bird_mask.overlap(bottom_mask, bottom_distance)
+        
+        if top_colision or bottom_colision:
+            return True
+        else:
+            return False
+        
 Class Base:
     pass
